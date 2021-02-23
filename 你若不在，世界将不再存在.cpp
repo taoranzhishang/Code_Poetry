ed1fd53ddc21087a1c6926e0e6e4f21a5fedfd59
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 19:54:56 +0800

    Add files via upload

---
+++ 你若不在，世界将不再存在.cpp
@@ -0,0 +1,13 @@
+if (!MyLife.you) {
+
+      MyLife.days = null;
+
+      MyLife.nights.lastingTime = long.MaxValue; }
+
+/*
+   If I had to live my life without you near me,
+
+   The days would all be empty,
+
+   The nights will seem so long.
+*/
