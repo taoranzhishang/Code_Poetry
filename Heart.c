b79f60f6e7872fd2a22d029e74566b08c5e83ba1
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 18:37:30 +0800

    Add files via upload

---
+++ Heart.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+	for (float y = 2; y > -2; (y -= 0.1) && (putchar('\n')))
+
+		for (float x = -2; x < 2; x += 0.05)
+
+			putchar((x*x + y * y - 1)*(x*x + y * y - 1)*(x*x + y * y - 1) - x * x*y*y*y <= 0 ? '*' : ' ');
+}
