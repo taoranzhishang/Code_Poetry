91ed98abcf6b0995a49e06f399c80d2ecc4c6388
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 19:50:34 +0800

    Add files via upload

---
+++ 爱你成为永恒.c
@@ -0,0 +1,7 @@
+const int My_Love_For_You = 1;    //爱你成了一种永恒
+
+while(My_Love_For_You)          //我想用对你永恒的爱
+
+for(My_Love = ravishing_you; My_Love <= aged_you;);
+
+//见证我从爱你的年轻貌美，直到爱你的花白鬓角,但我的爱，从未改变
