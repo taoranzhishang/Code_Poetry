2e9cb723438039c69e9a43b7641c9d51691963ff
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 20:33:43 +0800

    Add files via upload

---
+++ Catch The Wind.java
@@ -0,0 +1,36 @@
+try {
+	try {
+		In the chilly hours and minutes
+		Of uncertainty, I want to be
+		In the warm hold of your loving mind
+
+		To feel you all around me
+		And to take your hand along the sand
+	}
+	catch (the wind) {}
+ 
+	try {
+		When sundown pales the sky
+		I want to hide a while behind your smile
+		And everywhere I'd look your eyes I'd find
+
+		For me to love you now
+		Would be the sweetest thing 'twould make me sing
+	}
+	catch (the wind) {}
+
+	Di di di di, di di di di
+	Di di di di, di di di di
+	Di di di
+
+	try {
+		When rain has hung the leaves with tears
+		I want you near to kill my fears
+		To help me to leave all my blues behind
+
+		For standin' in your heart
+		Is where I want to be and long to be
+	}
+	catch (the wind) {}
+}
+catch (the wind) {}
