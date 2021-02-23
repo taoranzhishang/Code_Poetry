40344c87f997ce6059570c2bd754948b3f1ed4d7
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 20:59:20 +0800

    Add files via upload

---
+++ 想你了.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+   两隔的世界; 无名的信件; 短暂的停留; 长久的记忆; 说这么多，只是想说：我想你了。
+*/
+int main()
+{
+double world;
+unsigned letter;
+short stay;
+long memories;
+printf("I miss you.\n");
+return 0;
+}
