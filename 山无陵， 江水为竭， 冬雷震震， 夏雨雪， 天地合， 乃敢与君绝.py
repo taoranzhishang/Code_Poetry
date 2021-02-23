a8f0adfce055bd84060b529773cc8d2f3678f613
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 20:50:50 +0800

    Add files via upload

---
+++ 山无陵， 江水为竭， 冬雷震震， 夏雨雪， 天地合， 乃敢与君绝.py
@@ -0,0 +1,12 @@
+"""
+     山无陵， 江水为竭， 冬雷震震， 夏雨雪， 天地合， 乃敢与君绝！
+"""
+
+if(mountain.arris==None):
+    if(river.water==None):
+        if(winter.thunder==True):
+            if(summer.snow==True):
+                if(sky.height==ground.height):
+                    i.withyou=False
+else:
+    i.withyou=True
