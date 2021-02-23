c1686672100b8a0b38f31db1faa77c513a5e66c3
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 18:56:30 +0800

    Add files via upload

---
+++ Untitled_1.cpp
@@ -0,0 +1,13 @@
+/*
+
+    #define THE_EXISTENCE_OF_YOU
+
+    void tendresse(std::map<int, string> destiny, int weight, int value, string boy, string girl);
+
+*/
+
+#ifdef THE_EXISTENCE_OF_YOU
+
+#define the_meaning_of_me(destiny, w,  v, boy, girl)   tendresse(dest, w, v, "I", "U");
+
+#endif
