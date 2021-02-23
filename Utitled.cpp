1ccb8741e89abc3b74194184e7753732a4da18f7
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 19:01:02 +0800

    Zhenhua Sheng

--- Utitled.cpp
+++ Utitled.cpp
@@ -1,4 +1,4 @@
-char iLoveU[11] = "I Love You", char threeLine[999], char aLife[2];   //我爱你
+char iLoveU[11] = "I Love You", char threeLine[999], char aLife[2];  //我爱你
 
 strcpy(threeLine, iLoveU);  //三行太长
 
