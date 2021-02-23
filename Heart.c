2aa9db4234b048e9db29dd0373e7b166dd39f7d9
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-23 19:08:15 +0800

    Yuchen Wang

--- Heart.c
+++ Heart.c
@@ -3,8 +3,6 @@
 void main()
 {
 	for (float y = 2; y > -2; (y -= 0.1) && (putchar('\n')))
-
 		for (float x = -2; x < 2; x += 0.05)
-
 			putchar((x * x + y * y - 1) * (x * x + y * y - 1) * (x * x + y * y - 1) - x * x * y * y * y <= 0 ? '*' : ' ');
 }
