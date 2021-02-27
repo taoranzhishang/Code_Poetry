0c16d7fe6f0b4afc44dc72db5b5661e638f8981a
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-27 19:07:11 +0800

    Add files via upload

---
+++ 真爱永恒.java
@@ -0,0 +1,12 @@
+/**
+     * @author 文艺范码农
+     * @description 代码情诗
+     * @date 2017年11月7日
+     */
+    private void love(int you) {
+        int i = you;         //一见钟情
+        you = i;             //相濡以沫
+        if(i==you) {         //不忘初心
+            love(1);         //真爱永恒
+        }
+    }
