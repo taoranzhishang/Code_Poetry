c03e44c2e116ea0e801f5be931da334b5ef51409
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-02-28 21:15:54 +0800

    Add files via upload

---
+++ 明日复明日.cpp
@@ -0,0 +1,10 @@
+/*
+* 明日复明日 明日何其多。我生待明日，万事成蹉跎。
+*/
+
+while(tomorrow.hasNext()){
+          howMany(tomorrow); 
+          tomorrow.next();
+        }
+this.wait(WHOLE_LIFE);
+return (null == everything)?null:null;
