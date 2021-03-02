cf1fb60bfe0a24bc19016c319c044395ae10be10
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-03-02 21:49:38 +0800

    Add files via upload

---
+++ 爱一个人成空.py
@@ -0,0 +1,17 @@
+"""
+喜欢爱一个人，一辈子成空
+"""
+
+def like(lover):
+     if lover == you:
+            love(lover).forever
+     else:
+            global live
+            live = None
+
+
+live = True
+
+while live:
+        like(you)
+        like(None)
