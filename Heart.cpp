6e82f0c50eadccf61c1aab22632750fa15722447
Colder <61004015+taoranzhishang@users.noreply.github.com>
2021-03-14 21:45:51 +0800

    Add files via upload

---
+++ Heart.cpp
@@ -0,0 +1,28 @@
+    
+              #include      <iostream>
+
+         int  main() {int A = 43,B = A / 2,
+
+    C = B / 2, x,y,i = B,j =-C; for(y = 0; y < B;
+
+   j = (++y) - (C + C / 8)) {for(x = 0; x < A; i =
+
+	  abs((++x) - B) + j) {std::cout << ((i * i
+
+        / 2 + j * j<(C*C*2)/3)?"*" : " "); }
+
+    	       std::cout << std::endl
+
+		           ;  }  return
+
+			            0;}
+
+
+
+
+
+
+
+
+
+
